d3c13ad003f9a5fe33e3378b40d600a6a3894182
stensootla <stensootla@gmail.com>
2018-08-06 16:36:45 +0100

    updated requirements and added command line arguments to preprocessing script

--- scripts/celeba_data_preprocess.py
+++ scripts/celeba_data_preprocess.py
@@ -1,26 +1,39 @@
 """Resizes and crops CelebA 178x128 images to 64x64."""
 import os
+import argparse
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from scipy.misc import imresize
 from PIL import Image
 
 
-IMGDIR = '/home/stensootla/projects/celeba/img_align_celeba/'
-SAVE_IMGDIR = '/home/stensootla/projects/celeba/resized/'
+def parse():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--source-dir', type=str, required=True,
+                      help='path to directory where the original images reside')
+  parser.add_argument('--target-dir', type=str, required=True,
+                      help='path to directory where the resized images will be \
+                            saved')
+  return parser.parse_args()
 
 
-if not os.path.isdir(SAVE_IMGDIR):
-  os.mkdir(SAVE_IMGDIR)
-img_list = os.listdir(IMGDIR)
+def preprocess(args):
+  if not os.path.isdir(args.target_dir):
+    os.mkdir(args.target_dir)
+  img_list = os.listdir(args.source_dir)
 
-for i, filename in enumerate(tqdm(img_list)):
-  img_path = os.path.join(IMGDIR, filename)
-  img = Image.open(img_path) \
-          .resize((64, 78), Image.ANTIALIAS) \
-          .crop((0, 7, 64, 64 + 7))
+  for i, filename in enumerate(tqdm(img_list)):
+    src_img_path = os.path.join(args.source_dir, filename)
+    img = Image.open(src_img_path) \
+            .resize((64, 78), Image.ANTIALIAS) \
+            .crop((0, 7, 64, 64 + 7))
   
-  save_path = os.path.join(SAVE_IMGDIR, filename)
-  with open(save_path, 'w') as f:
-    img.save(f)
-  
+    target_img_path = os.path.join(args.target_dir, filename)
+    with open(target_img_path, 'w') as f:
+      img.save(f)
+
+
+if __name__ == '__main__':
+  args = parse()
+  preprocess(args)
+
